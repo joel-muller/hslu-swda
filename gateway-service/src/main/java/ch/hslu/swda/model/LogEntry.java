dcd3a02086b45a68a5287c43c701a00eabd0011b
Jeremy Funke <jeremy.funke@stud.hslu.ch>
2024-10-30 16:12:41 +0100

    Make LogEntry (de)serializeable

--- gateway-service/src/main/java/ch/hslu/swda/model/LogEntry.java
+++ gateway-service/src/main/java/ch/hslu/swda/model/LogEntry.java
@@ -2,8 +2,10 @@ package ch.hslu.swda.model;
 
 import java.util.UUID;
 import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
 
 @Introspected
+@Serdeable
 public record LogEntry(UUID id, String source, long timestamp, int userId, String eventType, UUID objUuid, String message) {
 
 }
