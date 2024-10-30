fce8ab7b333607eb03e1fc43e7ef7dc7ef90e536
Patrick Francke <patrick.francke@stud.hslu.ch>
2024-10-30 15:38:56 +0100

    make LogEntry introspected

--- gateway-service/src/main/java/ch/hslu/swda/model/LogEntry.java
+++ gateway-service/src/main/java/ch/hslu/swda/model/LogEntry.java
@@ -1,7 +1,9 @@
 package ch.hslu.swda.model;
 
 import java.util.UUID;
+import io.micronaut.core.annotation.Introspected;
 
+@Introspected
 public record LogEntry(UUID id, String source, long timestamp, int userId, String eventType, UUID objUuid, String message) {
 
 }
