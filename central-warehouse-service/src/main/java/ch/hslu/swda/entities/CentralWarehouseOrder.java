44ae9fdb56de22bfa45ec9b0aa0e6b7319d57b2b
Patrick Francke <patrick.francke@stud.hslu.ch>
2024-11-14 00:57:04 +0100

    missed in last commit. update CentralWarehouseOrder

--- central-warehouse-service/src/main/java/ch/hslu/swda/entities/CentralWarehouseOrder.java
+++ central-warehouse-service/src/main/java/ch/hslu/swda/entities/CentralWarehouseOrder.java
@@ -15,20 +15,18 @@
  */
 package ch.hslu.swda.entities;
 
-import ch.hslu.swda.stock.api.Stock;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 
-/**
- * Einfaches Datenmodell eines Studenten.
- */
+
 public final class CentralWarehouseOrder {
 
     @JsonProperty("id")
@@ -36,24 +34,22 @@ public final class CentralWarehouseOrder {
     @JsonProperty("storeId")
     private UUID storeId;
     @JsonProperty("articles")
-    private Map<Integer,Integer> articles;
+    private List<OrderArticle> articles;
+
 
     private static final Logger LOG = LoggerFactory.getLogger(CentralWarehouseOrder.class);
 
-    private Stock stock;
 
     /**
      * Default Konstruktor.
      */
-    public CentralWarehouseOrder(UUID storeId, Map<Integer,Integer> articles, Stock stock) throws IllegalArgumentException  {
+    public CentralWarehouseOrder(UUID storeId, Map<Integer,Integer> articles) throws IllegalArgumentException  {
         this.id = UUID.randomUUID();
         this.storeId = storeId;
-        validateArticles(articles);
-        this.articles = articles;
-        this.stock = stock;
+        articles.forEach((k,v)->{
+                this.articles.add(new OrderArticle(k,v));
+            });
     }
-
-
 
     /**
      * @return the id
@@ -61,77 +57,12 @@ public final class CentralWarehouseOrder {
     public UUID getId() {
         return this.id;
     }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(final UUID id) {
-        this.id = id;
-    }
-
-    /**
-     * @return the firstName
-     */
-    public Map<Integer,Integer> getArticles() {
+    public List<OrderArticle> getArticles(){
         return this.articles;
     }
 
     /**
-     * @param articleId the id of the article
-     * @param count the amount of articles of that id to add
-     */
-    public int addArticle(int articleId, int count) {
-        if (this.articles.containsKey(articleId)){
-            this.articles.put(articleId, this.articles.get(articleId)+count);
-        }else{
-            this.articles.put(articleId,count);
-        }
-        return this.articles.get(articleId);
-    }
-
-    /**
-     * @param articles map to check for validity
-     */
-    public static void validateArticles(Map<Integer,Integer> articles) throws IllegalArgumentException{
-        articles.forEach((k,v)->{
-            if(v<1){
-                throw new IllegalArgumentException("Cannot add article to order with count <1. Actual count: " +v+" for articleId "+k);
-            }
-            if(k<100000){
-                throw new IllegalArgumentException("Cannot create order with articleId < 100'000 Actual articleID: " +k);
-            }
-        }
-        );
-        LOG.info("Verified warehouse articles for processing");
-    }
-
-
-    /**
-     * @param articleId the id of the article
-     * @param count the amount of articles of that id to remove
-     * @return the amount of articles with articleId in warehouse order
-     */
-    public int removeArticle(int articleId, int count){
-        if(this.articles.containsKey(articleId)){
-            int currentCount = this.articles.get(articleId);
-            int countAfter = currentCount-count;
-            if(countAfter<=0){
-                this.articles.remove(articleId);
-                return 0;
-            }
-            this.articles.put(articleId,countAfter);
-        }else{
-            return 0;
-        }
-
-
-        return this.articles.get(articleId);
-    }
-
-
-
-    /**
-     * Studenten mit identischer ID sind gleich. {@inheritDoc}.
+     * Orders mit identischer ID sind gleich. {@inheritDoc}.
      */
     @Override
     public boolean equals(final Object obj) {
@@ -150,32 +81,8 @@ public final class CentralWarehouseOrder {
         return Objects.hash(this.id);
     }
 
-    public void process(){
-        this.articles.forEach((k,v)->{
-            int ordered = stock.orderItem(k,v);
-            String reserved = stock.reserveItem(k,v);
-            if(reserved==null){
-                LOG.info(("Could not reserve " + v+" items of articleId "+ k));
-            }else{
-                UUID wareHouseOrderItemReservationId = UUID.fromString(reserved);
-                LOG.info("Reserved " + v+" items of articleId "+ k+". UUID: "+wareHouseOrderItemReservationId);
-            }
-            switch (ordered){
-                case 0:
-                    LOG.info("article "+ k +" not available in quantity " + v);
-                    break;
-                case -1:
-                    LOG.error("invalid articleId: "+ k);
-                    break;
-                default:
-                    LOG.info("ordered " +ordered +" of articleId "+ k);
-
-            }
-        });
-    }
-
     /**
-     * Liefert eine String-Repräsentation . {@inheritDoc}.
+     * Liefert eine String-Repräsentation. {@inheritDoc}.
      */
     @Override
     public String toString() {
