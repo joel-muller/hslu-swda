20c09b6d2ccb737381ed9e27d72617ffa937fbca
Jae <39260243+Spoicy@users.noreply.github.com>
2024-11-27 11:40:09 +0100

    Add message structure from other services

---
+++ authentication-service/src/main/java/ch/hslu/swda/messages/UserCreate.java
@@ -0,0 +1,49 @@
+package ch.hslu.swda.messages;
+
+import java.util.Objects;
+
+public class UserCreate implements IngoingMessage {
+    private final String username;
+    private final String password;
+    private final String role;
+
+    public UserCreate(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserCreate that = (UserCreate) o;
+        return Objects.equals(username, that.username) && Objects.equals(password, that.password) && Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return "UserCreate{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
