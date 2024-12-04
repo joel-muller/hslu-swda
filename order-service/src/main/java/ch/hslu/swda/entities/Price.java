fcc61e70807a4e7c8d38a8936d25d88228220c31
Joel-Mueller <80531955+Joel-Mueller@users.noreply.github.com>
2024-12-04 13:40:03 +0100

    Add price and also unit thests for that

---
+++ order-service/src/main/java/ch/hslu/swda/entities/Price.java
@@ -0,0 +1,58 @@
+package ch.hslu.swda.entities;
+
+
+import java.util.List;
+import java.util.Objects;
+
+public final class Price {
+    private final int francs;
+    private final int centimes;
+
+    public Price(int francs, int centimes) {
+        this.francs = francs + (centimes / 100);
+        if (centimes % 5 != 0 || centimes < 0 || francs < 0) {
+            throw new IllegalArgumentException("Francs and Centimes should be over 0 and Centimes should be divisible through 5");
+        }
+        this.centimes = centimes % 100;
+    }
+
+    public static Price generateFromList(List<String> list) throws NumberFormatException {
+        int francs = Integer.parseInt(list.get(1));
+        int centimes = Integer.parseInt(list.get(2));
+        return new Price(francs, centimes);
+    }
+
+    public int getFrancs() {
+        return francs;
+    }
+
+    public int getCentimes() {
+        return centimes;
+    }
+
+    public Price addAnother(Price price) {
+        int francs = this.francs + price.getFrancs() + ((this.centimes + price.getCentimes()) / 100);
+        int centimes = (this.centimes + price.getCentimes()) % 100 ;
+        return new Price(francs, centimes);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Price price)) return false;
+        return francs == price.francs && centimes == price.centimes;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(francs, centimes);
+    }
+
+    @Override
+    public String toString() {
+        if (centimes < 10) {
+            return "Price{" + this.francs + ".0" + this.centimes + " Francs}";
+        }
+        return "Price{" + this.francs + "." + this.centimes + " Francs}";
+    }
+}
