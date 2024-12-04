7dc52f23ead7e92b884f2c389e3415fc5f8d99dd
Joel-Mueller <80531955+Joel-Mueller@users.noreply.github.com>
2024-12-04 11:05:23 +0100

    Change verifyresponse message

--- order-service/src/main/java/ch/hslu/swda/messagesIngoing/VerifyResponse.java
+++ order-service/src/main/java/ch/hslu/swda/messagesIngoing/VerifyResponse.java
@@ -1,8 +1,9 @@
 package ch.hslu.swda.messagesIngoing;
 
+import java.util.Map;
 import java.util.UUID;
 
-public record VerifyResponse(UUID idOrder, boolean valid) implements IngoingMessage {
+public record VerifyResponse(UUID idOrder, boolean valid, Map<Integer, Integer> francsPerUnit, Map<Integer, Integer> centimesPerUnit) implements IngoingMessage {
 
     @Override
     public UUID getOrderId() {
