f32853c1ec48820f0621191e3093b1dad284ea1b
Joel-Mueller <80531955+Joel-Mueller@users.noreply.github.com>
2024-12-11 21:27:07 +0100

    Add new logs for order cancelled

--- store-management-service/src/main/java/ch/hslu/swda/business/HandleOrderCancelled.java
+++ store-management-service/src/main/java/ch/hslu/swda/business/HandleOrderCancelled.java
@@ -10,16 +10,23 @@ import ch.hslu.swda.persistence.DatabaseConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 public class HandleOrderCancelled implements Modifiable {
     private static final Logger LOG = LoggerFactory.getLogger(HandleOrderCancelled.class);
     @Override
     public void modify(Data databaseConnector, IngoingMessage responseRaw, Service service) {
-        OrderCancelled response = (OrderCancelled) responseRaw;
-        Store store = databaseConnector.getStore(response.getStoreId());
-        if (store != null) {
-            store.cancelOrder(response.orderId());
-            LOG.info("Order cancelled with the id {}, articles reserved from order are now part of the order inventory", response.orderId());
-            databaseConnector.storeStore(store);
+        try {
+            OrderCancelled response = (OrderCancelled) responseRaw;
+            Store store = databaseConnector.getStore(response.getStoreId());
+            if (store != null) {
+                store.cancelOrder(response.orderId());
+                service.log(new LogMessage(response.orderId(), store.getId(), "storemanagement.ordercancelled", "order with the id " + response.orderId().toString() + " is cancelled and will get deleted in this service"));
+                LOG.info("Order cancelled with the id {}, articles reserved from order are now part of the order inventory", response.orderId());
+                databaseConnector.storeStore(store);
+            }
+        } catch (IOException e) {
+            LOG.error("An error occurred while trying to cancel the order {}", e.getMessage());
         }
     }
 }
